0bbe061505ec7e113d23a320835c5cbe891e6dae
shyam-30 <108927290+shyam-30@users.noreply.github.com>
2022-08-15 11:44:17 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+
+const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://functionup-cohort:3bMssIUqmBmIqb3e@cluster0.3ccgp0v.mongodb.net/PK-100?retryWrites=true&w=majority"
+, {
+   useNewUrlParser: true 
+}
+).then( () => {console.log( "MongoDb is connected")}  )
+.catch( err => console.log(err))
+
+
+
+
+app.use('/', route);
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
+
